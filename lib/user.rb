2588a6bb828e59a245e0a53ca35fcdc1b25e83f3
Nadia Odunayo <nodunayo@gmail.com>
2013-11-07 13:22:25 +0000

    Added password confirmation feature

--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,6 @@
 # bcrypt will generate the password hash
 require 'bcrypt'
+require 'dm-validations'
 class User
 
   include DataMapper::Resource
@@ -8,7 +9,13 @@ class User
   property :email, String
   property :password_digest, Text
 
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  validates_confirmation_of :password
+
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
